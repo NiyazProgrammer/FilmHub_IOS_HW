d10c5b9489f92ea6762b3fef599f896901f3a0db
NiyazProgrammer <126018017+NiyazProgrammer@users.noreply.github.com>
2024-04-07 15:32:05 +0300

    Revert "Add my files screen"

--- FilmHub_IOS_HW/FilmHub_IOS_HW/ProfilePage/DataManager/ProfilesMoviewsDataManager.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import UIKit
-
-class ProfilesMoviewsDataManager {
-    var favoriteFilmsId: [Int] = [300, 355, 480, 550]
-    var RecentFilmsId: [Int] = [330, 345, 680, 750, 555, 384, 465, 660, 1000]
-}
--- FilmHub_IOS_HW/FilmHub_IOS_HW/ProfileScreen/ProfileViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProfileViewController.swift
-//  FilmHub_IOS_HW
-//
-//  Created by Нияз Ризванов on 07.04.2024.
-//
-
-import Foundation
